6af88eccff266c8d3cb7ac37bd74e00907b8d0fb
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-05-17 21:02:03 -0700

    building nav

---
+++ react-app/src/components/LowerNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LowerNav = () => {
+  return (
+    <nav>
+        
+    </nav>
+  )
+}
+
+export default LowerNav;
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,36 +2,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './navBar.css';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className='nav-container'>
+        <ul className='unordered-list'>
+          <li className='list-item'>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li className='list-item'>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Login
+            </NavLink>
+          </li>
+          <li className='list-item'>
+            <NavLink to='/sign-up' exact={true} activeClassName='active'>
+              Sign Up
+            </NavLink>
+          </li>
+          <li className='list-item'>
+            <NavLink to='/users' exact={true} activeClassName='active'>
+              Users
+            </NavLink>
+          </li>
+          <li className='list-item'>
+            <LogoutButton />
+          </li>
+        </ul>
+      </nav>
+
+    </>
   );
 }
 

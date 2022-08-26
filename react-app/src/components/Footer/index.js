baa5c71a716b9fc270d3da8697a4c439780c327f
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-08-26 08:34:39 -0700

    implementing footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,25 +1,78 @@
 import React from 'react'
-import './footer.css'
+import { NavLink } from 'react-router-dom';
+import './lowerFooter.css'
 
 function UpperFooter() {
     return (
         <div className='footer-container'>
             <div>
-                <button>Back to top</button>
+                <button className='backToTopBtn'>Back to top</button>
             </div>
-            <div>
+            <div className='upperFooterColumns'>
+                <div className='upColumns'>
+                    <h4>Get to Know Me</h4>
+                    <div className='upFootBtnContainer'>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                    </div>
+                </div>
+                <div className='upColumns'>
+                    <h4>I'd like to work for you</h4>
+                    <div className='upFootBtnContainer'>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                    </div>
+                </div>
+                <div className='upColumns'>
+                    <h4>Let Me Work for You</h4>
+                    <div className='upFootBtnContainer'>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+
+                    </div>
+                </div>
+                <div className='upColumns'>
+                    <h4>Let Me Work for You</h4>
+                    <div className='upFootBtnContainer'>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                        <NavLink className="upperFooterLink" to=''>Hi</NavLink>
+                    </div>
+                </div>
 
             </div>
-            <div>
-
-            </div>
-            <div>
-
-            </div>
-            <div>
-
-            </div>
-            Footer
+                <div className='logoDiv'>
+                    <NavLink to='/'>
+                    <img className='upperFooterLogo' src='https://raw.githubusercontent.com/k-dodsonknapp/mmazon_clone/main/mmazon-logo-white-blue-backg.png'/>
+                    </NavLink>
+                </div>
         </div>
     )
 }
